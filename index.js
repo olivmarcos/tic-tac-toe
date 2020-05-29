123cae5ce691241e46111360216904596c38793c
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2020-05-29 08:33:41 -0300

    fix typos

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const gameBoard = (() => {
   const _board = document.querySelector("#gameBoard");
-  const _boradArray = ["", "", "", "", "", "", "", "", ""];
+  const _boardArray = ["", "", "", "", "", "", "", "", ""];
 
   function render() {
-      let i = 0;
-    _boradArray.forEach((item) => {
+      let i = 0; //only for test
+      _boardArray.forEach((item) => {
       console.log(item);
       const _block = document.createElement("div");
       _block.classList.add("block");
