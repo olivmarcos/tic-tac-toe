cdd04dd61270124f3b776d735e07faf7a6b708a8
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2020-06-01 08:14:16 -0300

    verify the winner

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const gameBoard = (() => {
   const _board = document.querySelector("#gameBoard");
-  let boardArray = ["", "", "", "", "", "", "", "", ""];
+  let boardArray = [4, 9, 2, 3, 5, 7, 8, 1, 6];
 
   const render = () => {
     let count = 0;
@@ -22,25 +22,36 @@ const Player = (name, sign) => {
 
 const game = (() => {
   const player1 = Player("Player1", "X");
-  const player2 = Player("Player1", "O");
+  const player2 = Player("Player2", "O");
 
   const _turn = () => {
     let currentPlayer = 1;
+    let playerOneArr = [];
+    let playerTwoArr = []
 
     document.addEventListener("click", (e) => {
       if (!e.target.matches(".block")) return;
+
       if (currentPlayer == 1 && e.target.innerHTML === "") {
         e.target.innerHTML = player1.sign;
-        gameBoard.boardArray[e.target.getAttribute('value')] = player1.sign;
+        playerOneArr.push(gameBoard.boardArray[e.target.getAttribute('value')]);
+        if(_checkWin(playerOneArr)) console.log(`${player1.name} is the winner!`);
         currentPlayer = 2;
       } else if (currentPlayer == 2 && e.target.innerHTML === "") {
         e.target.innerHTML = player2.sign;
-        gameBoard.boardArray[e.target.getAttribute('value')] = player2.sign;
+        playerTwoArr.push(gameBoard.boardArray[e.target.getAttribute('value')]);        
         currentPlayer = 1;
+        if(_checkWin(playerTwoArr)) console.log(`${player2.name} is the winner!`);
       }
-      console.log(gameBoard.boardArray);
     });
   };
+
+  const _checkWin = (array) => {
+    const add = (a , b) => a + b;
+    const sum = array.reduce(add);
+    
+    return sum === 15;
+  }
 
   const startGame = () => {
     _turn();
