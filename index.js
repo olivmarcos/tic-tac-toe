adfceb09ce416137908709e7f4455a9363007d44
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2020-06-06 16:31:06 -0300

    finish

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ const game = (() => {
   let playerOneCount = 0;
   let playerTwoCount = 0;
 
+  let scores = document.querySelectorAll(".score");
+  scores[0].innerHTML = `Score: ${playerOneCount}`;
+  scores[1].innerHTML = `Score: ${playerTwoCount}`;
+
   const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -43,36 +47,33 @@ const game = (() => {
   ];
 
   const _turn = () => {
-    let scores = document.querySelectorAll(".score");
-    scores[0].innerHTML = `Score: ${playerOneCount}`;
-    scores[1].innerHTML = `Score: ${playerTwoCount}`;
-
     document.addEventListener("click", (e) => {
       if (!e.target.matches(".block")) return;
 
       if (currentPlayer == 1 && e.target.innerHTML === "") {
+        count++;
         e.target.innerHTML = _setPlayersName().player1.sign;
         playerOneArr.push(parseInt(e.target.getAttribute("value")));
 
         if (_checkWin(playerOneArr)) {
           _congrastTheWinner(_setPlayersName().player1);
-          scores[0].innerHTML = `Score: ${playerOneCount += 1}`;
+          scores[0].innerHTML = `Score: ${(playerOneCount += 1)}`;
           playerOneWon = true;
         }
 
         currentPlayer = 2;
       } else if (currentPlayer == 2 && e.target.innerHTML === "") {
+        count++;
         e.target.innerHTML = _setPlayersName().player2.sign;
         playerTwoArr.push(parseInt(e.target.getAttribute("value")));
 
         if (_checkWin(playerTwoArr)) {
           _congrastTheWinner(_setPlayersName().player2);
-          scores[1].innerHTML = `Score: ${playerTwoCount += 1}`;
+          scores[1].innerHTML = `Score: ${(playerTwoCount += 1)}`;
           playerTwoWon = true;
         }
         currentPlayer = 1;
       }
-      count++;
       if (!playerOneWon && !playerTwoWon && count == 9) _announceTie();
     });
   };
@@ -83,6 +84,8 @@ const game = (() => {
   const _congrastTheWinner = (player) => {
     const div = document.querySelector(".winner");
     const h1 = document.querySelector(".winner h1");
+    const gameBoard = document.querySelector("#gameBoard");
+    gameBoard.classList.add("noClick");
     div.style.visibility = "visible";
     h1.innerHTML = `${player.name} won!`;
   };
@@ -95,7 +98,7 @@ const game = (() => {
   };
 
   const resetGame = () => {
-    const button = document.querySelector(".btn");
+    const buttons = document.querySelectorAll(".reset");
 
     const _setInvisible = () => {
       const winnerDiv = document.querySelector(".winner");
@@ -114,14 +117,22 @@ const game = (() => {
       playerTwoArr = [];
     };
 
-    button.addEventListener("click", () => {
-      _setInvisible();
-      _setBoardClean();
-      _setPlayersClean();
-      currentPlayer = 1;
-      count = 0;
-      playerOneWon = false;
-      playerTwoWon = false;
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const gameBoard = document.querySelector("#gameBoard");
+        gameBoard.classList.remove("noClick");
+        _setInvisible();
+        _setBoardClean();
+        _setPlayersClean();
+        currentPlayer = 1;
+        count = 0;
+        playerOneWon = false;
+        playerTwoWon = false;
+        if (button.innerHTML === "restart") {
+  scores[0].innerHTML = `Score: ${playerOneCount = 0}`;
+  scores[1].innerHTML = `Score: ${playerTwoCount = 0}`;
+        }
+      });
     });
   };
 
@@ -129,10 +140,10 @@ const game = (() => {
     const startButton = document.querySelector(".start button");
     startButton.addEventListener("click", () => {
       const divStart = document.querySelector(".start");
-      const gameBoard = document.querySelector("#gameBoard");
+      const board = document.querySelector(".board");
       const displays = document.querySelector("#displays");
       divStart.style.visibility = "hidden";
-      gameBoard.style.visibility = "visible";
+      board.style.visibility = "visible";
       displays.style.visibility = "visible";
       _setPlayersName();
       _turn();
@@ -143,8 +154,14 @@ const game = (() => {
 
   const _setPlayersName = () => {
     const players = document.querySelectorAll(".playersName input");
-    const player1 = Player(!(players[0].value) ? 'Player 1': players[0].value, "X");
-    const player2 = Player(!(players[1].value) ? 'Player 2': players[1].value, "O");
+    const player1 = Player(
+      !players[0].value ? "Player 1" : players[0].value,
+      "X"
+    );
+    const player2 = Player(
+      !players[1].value ? "Player 2" : players[1].value,
+      "O"
+    );
 
     return { player1, player2 };
   };
